8b0aa6d0217823d7a8f21c85350e2890fb407204
Victor Hugo Assumpção De Amorim <110578804+Victor2212-code@users.noreply.github.com>
2025-03-15 14:13:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "local" {}
 
-resource "null_resource" "create_k3d_cluster"{
-    provisioner "local-exec" {
-        command = "k3d cluster create meu-cluster --agents 2"
-    }
+resource "null_resource" "create_k3d_cluster" {
+  provisioner "local-exec" {
+    command = <<EOT
+      k3d cluster delete meu-cluster || true
+      k3d cluster create meu-cluster --agents 2
+    EOT
+  }
 }
