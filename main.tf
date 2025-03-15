87f73c03ac35a9cffbd300eac4aecda015958735
Victor2212-code <110578804+Victor2212-code@users.noreply.github.com>
2025-03-15 14:02:42 -0300

    Commit intermediário

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "local" {}
 
 resource "null_resource" "create_k3d_cluster"{
     provisioner "local-exec" {
-        command = "k3d cluster create meu-cluster --agent 2"
+        command = "k3d cluster create meu-cluster --agents 2"
     }
 }
